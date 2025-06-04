69a77cfff478facc811fde4c94d4d9bb44f08975
Andriusmw <andriusmw@yahoo.es>
2025-06-04 20:30:05 +0200

    testing nextjs routing

---
+++ app/noticias/page.tsx
@@ -0,0 +1,7 @@
+export default async function Noticias() {
+    return (
+        <section>
+            <p>Noticias</p>
+        </section>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,11 @@ import { ThemeSwitcher } from "@/components/theme-switcher";
 export default function Home() {
   return (
   
-      <main className="">
-        <h1>Hello world!</h1>
-        <ThemeSwitcher></ThemeSwitcher>
-      </main>
+    
+
+      <section>
+        probando
+      </section>
         
    
   );
